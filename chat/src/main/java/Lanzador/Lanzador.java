26f476f1bf83790a9d4507ab97118f5bba50db5f
MSI <MSI@172.22.75.132>
2024-11-13 10:32:53 +0100

    Empezamos con DAO, lanzador del Login hecho e intentando la parte de CHAT

--- chat/src/main/java/Lanzador/Lanzador.java
+++ chat/src/main/java/Lanzador/Lanzador.java
@@ -1,9 +1,16 @@
 package Lanzador;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import Aplicacion.Login;
 
 public class Lanzador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Login login = new Login();
+		DateTimeFormatter solo_hora = DateTimeFormatter.ofPattern("HH:mm");
 
+		//System.out.println(LocalDateTime.now().format(solo_hora));
 	}
 }
